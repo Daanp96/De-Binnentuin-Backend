d087166a5badcfdb5e5fc714f5def2ccd5629c02
Warner Hoefakker <warner.hoefakker@live.nl>
2020-06-10 09:27:06 +0200

    Login

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+  Route::post('login', [ 'as' => 'login', 'uses' => 'LoginController@do']);
+  Auth::routes();
     return view('welcome');
 });
