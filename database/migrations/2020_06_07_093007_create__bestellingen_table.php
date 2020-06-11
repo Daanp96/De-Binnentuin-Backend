a6a859ee52a315fd6c81ce95cde02e1cdffe2bc2
Thomas Soeterbroek <thosoevel@gmail.com>
2020-06-11 11:58:48 +0200

    Bestelling migration voor Marco

--- database/migrations/2020_06_07_093007_create__bestellingen_table.php
+++ database/migrations/2020_06_07_093007_create__bestellingen_table.php
@@ -20,6 +20,8 @@ class CreateBestellingenTable extends Migration
             $table->boolean('betaald');
             $table->bigInteger('tafeltimeslots_id')->unsigned();
             $table->foreign('tafeltimeslots_id')->references('id')->on('tafel_timeslots');
+            $table->string('opmerking')->nullable();
+            $table->float('prijsVoledigeBestelling');
             $table->integer("hoeveelMensen");
         });
     }
